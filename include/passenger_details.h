a77cc126b449fb7f7da3eb171f8ee2fd0ccc646f
Rayasam Thej Prabhath <thejrayasam@gmail.com>
2020-04-15 14:30:45 -0400

    Changed file to file2

--- include/passenger_details.h
+++ include/passenger_details.h
@@ -12,7 +12,7 @@
 /**
 * Declaring the name of the csv file
 */
-#define file "passenger_details.csv"
+#define file2 "passenger_details.csv"
 
 /**
  * @brief Adds the details of the passengers to "passenger_details.csv" file
